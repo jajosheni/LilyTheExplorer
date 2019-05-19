5be3c51faf5b3078b5e34299d18a665c7c185978
Sheni Hamitaj <shen.i@live.com>
2019-05-19 06:16:08 +0300

    design finished

--- app/src/main/java/com/lab/lilytheexplorer/MainApp.java
+++ app/src/main/java/com/lab/lilytheexplorer/MainApp.java
@@ -1,10 +1,12 @@
 package com.lab.lilytheexplorer;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -12,9 +14,28 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainApp extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SearchView searchView;
+    private ListView categoryListView, resultsListView;
+    private String[] categoryList, resultsList;
+    private ArrayAdapter<String> categoryAdapter, resultsAdapter;
+    private SeekBar seekBar;
+    private TextView radiusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,15 +56,91 @@ public class MainApp extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        categoryList = getResources().getStringArray(R.array.category);
+        categoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,categoryList);
+        categoryListView = (ListView) findViewById(R.id.categoryListView);
+        categoryListView.setAdapter(categoryAdapter);
+
+        categoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO Auto-generated method stub
+                String value=categoryAdapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+                //Hide Keyboard on click
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        });
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(Arrays.asList(categoryList).contains(query)){
+                    categoryAdapter.getFilter().filter(query);
+                }else{
+                    Toast.makeText(MainApp.this, "No results found", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                categoryAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        radiusTextView = (TextView) findViewById(R.id.radiusTextView);
+        radiusTextView.setText("Radius: " + seekBar.getProgress() + "m");
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressValue = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                radiusTextView.setText("Radius: " + progress + "m");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "seekbar touch started", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "seekbar touch stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        resultsListView = (ListView) findViewById(R.id.resultsListView);
+        resultsList = getResources().getStringArray(R.array.results);
+        resultsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,resultsList);
+        resultsListView.setAdapter(resultsAdapter);
     }
 
-    @Override
+        @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            new AlertDialog.Builder(this)
+                    .setIcon(R.mipmap.ic_launcher_round)
+                    .setTitle("Lily the Explorer")
+                    .setMessage("Are you leaving?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
         }
     }
 
